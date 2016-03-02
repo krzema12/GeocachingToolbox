a012f6038461c9d345c410147eae088197268736
Loic Ploumen <loicploumen@hotmail.com>
2016-03-02 21:29:43 +0100

    Add the ability to use a simple XML configuration file in the Example project. The credentials won't be saved in the sources anymore.

--- Examples/GeocachingComExamples.cs
+++ Examples/GeocachingComExamples.cs
@@ -8,77 +8,83 @@ using System.Threading.Tasks;
 
 namespace Examples
 {
-	class GeocachingComExamples
-	{
-		private GCClient client;
-		private const string Login = "YourLoginHere";
-		private const string Password = "YourPasswordHere";
+    class GeocachingComExamples
+    {
+        private GCClient client;
+        private readonly string Login;
+        private readonly string Password;
 
-		public void Run()
-		{
-			LogIn();
-			DisplayUserInfo();
-			ListNewestFoundGeocaches();
-			GetDetailsForNewestFoundGeocache();
+        public GeocachingComExamples(string login,string password)
+        {
+            Login = login;
+            Password = password;
+        }
 
-			Console.Write("\n\n");
-			Console.WriteLine("Press enter to close this window...");
-			Console.Read();
-		}
+        public void Run()
+        {
+            LogIn();
+            DisplayUserInfo();
+            ListNewestFoundGeocaches();
+            GetDetailsForNewestFoundGeocache();
 
-		private void LogIn()
-		{
-			client = new GCClient();
+            Console.Write("\n\n");
+            Console.WriteLine("Press enter to close this window...");
+            Console.Read();
+        }
 
-			try
-			{
-				client.Login(Login, Password);
-			}
-			catch (IncorrectCredentialsException e)
-			{
-				Console.WriteLine("You probably didn't enter your real credentials several lines above...");
-				throw e;
-			}
-		}
+        private void LogIn()
+        {
+            client = new GCClient();
 
-		private void DisplayUserInfo()
-		{
-			Console.WriteLine("Hello {0}! So far you've found {1} geocaches.", client.User.Name, client.User.FoundGeocachesCount);
-		}
+            try
+            {
+                client.Login(Login, Password);
+            }
+            catch (IncorrectCredentialsException e)
+            {
+                Console.WriteLine("You probably didn't enter your real credentials several lines above...");
+                throw;
+            }
+        }
 
-		private void ListNewestFoundGeocaches()
-		{
-			Console.WriteLine("Here are some geocaches you've recently found:");
+        private void DisplayUserInfo()
+        {
+            Console.WriteLine("Hello {0}! So far you've found {1} geocaches.", client.User.Name, client.User.FoundGeocachesCount);
+        }
 
-			var foundLogsNewestTen = client.GetFoundGeocaches<GCLog>().Take(10);
+        private void ListNewestFoundGeocaches()
+        {
+            Console.WriteLine("Here are some geocaches you've recently found:");
 
-			foreach (var log in foundLogsNewestTen)
-			{
-				var cache = log.Thing as GCGeocache;
-				Console.WriteLine("({0}) {1}", cache.Status, cache.Name);
-			}
-		}
+            var foundLogsNewestTen = client.GetFoundGeocaches<GCLog>().Take(10);
 
-		private void GetDetailsForNewestFoundGeocache()
-		{
-			var newestFoundLog = client.GetFoundGeocaches<GCLog>().FirstOrDefault();
+            foreach (var log in foundLogsNewestTen)
+            {
+                var cache = log.Thing as GCGeocache;
+                Console.WriteLine("({0}) {1}", cache.Status, cache.Name);
+            }
+        }
 
-			if (newestFoundLog == null)
-			{
-				return;
-			}
+        private void GetDetailsForNewestFoundGeocache()
+        {
+            var newestFoundLog = client.GetFoundGeocaches<GCLog>().FirstOrDefault();
 
-			var newestFoundCache = newestFoundLog.Thing as GCGeocache;
-			client.GetGeocacheDetails<GCGeocache>(newestFoundCache);
+            if (newestFoundLog == null)
+            {
+                return;
+            }
 
-			Console.WriteLine("Name:               {0}", newestFoundCache.Name);
-			Console.WriteLine("Type:               {0}", newestFoundCache.Type);
-			Console.WriteLine("Difficulty/Terrain: {0}/{1}", newestFoundCache.Difficulty, newestFoundCache.Terrain);
-			Console.WriteLine("Status:             {0}", newestFoundCache.Status);
-			Console.WriteLine("Is premium:         {0}", newestFoundCache.IsPremium);
-			Console.WriteLine("Owner:              {0}", newestFoundCache.Owner.Name);
-			Console.WriteLine("Hint:               {0}", newestFoundCache.Hint);
-			Console.WriteLine("Description (beginning):\n{0}", newestFoundCache.Description.Substring(0, 500));
-		}
-	}
+            var newestFoundCache = newestFoundLog.Thing as GCGeocache;
+            client.GetGeocacheDetails<GCGeocache>(newestFoundCache);
+
+            Console.WriteLine("Name:               {0}", newestFoundCache.Name);
+            Console.WriteLine("Type:               {0}", newestFoundCache.Type);
+            Console.WriteLine("Difficulty/Terrain: {0}/{1}", newestFoundCache.Difficulty, newestFoundCache.Terrain);
+            Console.WriteLine("Status:             {0}", newestFoundCache.Status);
+            Console.WriteLine("Is premium:         {0}", newestFoundCache.IsPremium);
+            Console.WriteLine("Owner:              {0}", newestFoundCache.Owner.Name);
+            Console.WriteLine("Hint:               {0}", newestFoundCache.Hint);
+            Console.WriteLine("Description (beginning):\n{0}", newestFoundCache.Description.Substring(0, 500));
+        }
+    }
 }
--- Examples/Program.cs
+++ Examples/Program.cs
@@ -3,16 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GeocachingToolbox.Opencaching;
 
 namespace Examples
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			var examples = new GeocachingComExamples();
-			//var examples = new OpencachingExamples();
-			examples.Run();
-		}
-	}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Load configuration
+            var config = LoadConfiguration();
+
+            // Geocaching.com 
+            //var examples = new GeocachingComExamples(config.GCLogin, config.GCPassword);
+
+            // Opencaching
+            ApiAccessKeysImpl apiKeys = new ApiAccessKeysImpl(config.OCConsumerKey, config.OCConsumerSecret);
+            var examples = new OpencachingExamples(apiKeys);
+
+            examples.Run();
+        }
+
+        private static GeoCachingToolboxConfig LoadConfiguration()
+        {
+            GeoCachingToolboxConfig config = null;
+            try
+            {
+                config = SerializationHelper.Deserialize<GeoCachingToolboxConfig>("Config.xml");
+            }
+            catch (Exception)
+            {
+                config = new GeoCachingToolboxConfig();
+                SerializationHelper.Serialize("Config.xml", config);
+            }
+            return config;
+        }
+    }
+
+    public class GeoCachingToolboxConfig
+    {
+        public string OCConsumerKey { get; set; }
+        public string OCConsumerSecret { get; set; }
+        public string GCLogin { get; set; }
+        public string GCPassword { get; set; }
+
+        public GeoCachingToolboxConfig()
+        {
+            OCConsumerKey = "insert your consumer key";
+            OCConsumerSecret = "insert your consumer secret";
+            GCLogin = "insert your geocaching.com login";
+            GCPassword = "insert your geocaching.com password";
+        }
+    }
+
 }
